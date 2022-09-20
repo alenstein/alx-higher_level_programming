043b84ee4b05faab7971e1f5bf21b635fb5c8bdb
Allen Sibanda <allenalaska1@gmail.com>
2022-09-20 10:20:19 -0700

    made corrections to task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         _ord = ord(i)
         if _ord > 96:
-            _ord = 65 +(_ord - 97)
+            _ord = 65 + (_ord - 97)
         print("{}".format(chr(_ord)), end="")
-    print()          
+    print()
