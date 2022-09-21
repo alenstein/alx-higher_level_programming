0c0579686bd242b12bb2e7bc97d7abafc131b1f2
Allen Sibanda <allenalaska1@gmail.com>
2022-09-21 09:10:04 -0700

    made corrections to taks 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,8 @@
 def print_last_digit(number):
     if number > 0:
         ld = number % 10
-        print(f"{ld:d}")
+        print("{:d}.format(i)", end="")
     else:
         ld = number % -10
-        print(f"{ld:d}")
+        print("{:d}".format(i), end="")
     return (ld)
