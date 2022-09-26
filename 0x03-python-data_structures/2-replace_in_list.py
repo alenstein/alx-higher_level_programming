06840f0482d236a4c47c3d4002c9d093eea63249
Allen Sibanda <allenalaska1@gmail.com>
2022-09-25 23:59:19 -0700

    finished task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
-def element_at(my_list, idx):
-    if not idx:
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
         return None
     elif idx > len(my_list):
         return None
     else:
         for index in range(len(my_list)):
             if index == idx:
-                return my_list[index]
+                my_list[index] = element
+                return my_list
