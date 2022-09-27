8d1d2f2a4f66225f40c335f885889eaca5f7911b
Allen Sibanda <allenalaska1@gmail.com>
2022-09-26 23:05:22 -0700

    made corrections to tasks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if not idx:
+    if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
         for index in range(len(my_list)):
