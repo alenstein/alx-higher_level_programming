d714ea0b1e1d87ece35a1b5c04b3384301321b44
Allen Sibanda <allenalaska1@gmail.com>
2022-09-27 10:19:47 -0700

    made corrections to task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
-        for col in range(len(matrix[0]):
+        for col in range(len(matrix[0])):
             if col != row[-1]:
-                print("{:d}".format(matrix[row][col], end=" ")
+                print("{:d}".format(matrix[row][col]), end=" ")
             else:
-                print("{:d}".format(matrix[row][col], end="")
+                print("{:d}".format(matrix[row][col]), end="")
+        print()
