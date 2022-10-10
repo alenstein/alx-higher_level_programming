57e6f0288069924093b67b73a552c0509e640bce
Allen Sibanda <allenalaska1@gmail.com>
2022-10-10 09:49:49 -0700

    made corrections to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         while index < x:
             print("{}".format(my_list[index]), end="")
-            index++
+            index += 1
     except IndexError:
         pass
     print()
