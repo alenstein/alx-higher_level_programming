1cd6e336b8d044acb5bf294b2142a003105d6978
Allen Sibanda <allenalaska1@gmail.com>
2022-10-17 10:49:44 -0700

    finished task 2

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+    Module with empty class for creating a rectangle.
+"""
+
+
+class Rectangle:
+    """
+        definition for empty class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ property for getting width value """
+        return self.__width
+
+    @setter
+    def width(self, value):
+        """ setter for width value """
+        if isinstance(value, int):
+            pass
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ property for getting height value """
+        return self.__height
+
+    @setter
+    def height(self, value):
+        """ setter for height value """
+        if isinstance(value, int):
+            pass
+        else:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
