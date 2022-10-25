e15953fe73483e0df633a344b4f3df3ea6572bf7
Allen Sibanda <allenalaska1@gmail.com>
2022-10-25 10:54:53 -0700

    made corrections to task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -15,5 +15,5 @@ def append_write(filename="", text=""):
         text: the string to be appended to the file.
     """
 
-    with opens(filename, 'a', encoding="UTF-8") as af:
-        return(af.write(text))
+    with open(filename, 'a', encoding="UTF-8") as afile:
+        return(afile.write(text))
