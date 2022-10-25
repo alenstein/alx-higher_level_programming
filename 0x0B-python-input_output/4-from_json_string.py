2bc367ade709a357c28d65099d78fcc3024e9d9e
Allen Sibanda <allenalaska1@gmail.com>
2022-10-25 11:05:20 -0700

    finished task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Module containing a function that returns an object (Python data structure)
+    represented by a JSON string:
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+        definition for from_json_string function.
+
+        Args:
+        my_str: string object to be serialised to json format.
+    """
+
+    return (json.loads(my_str)) 
