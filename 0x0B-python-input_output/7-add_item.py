6d096de10db247424a141434dc9b7128431bc9b5
Allen Sibanda <allenalaska1@gmail.com>
2022-10-27 05:43:05 -0700

    finished task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,19 @@
 
 import os
 import json
+import sys
+import os.path
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 lists_file = "add_item.json"
-lists  = []
+lists = []
 
 if os.path.exists(lists_file) and os.path.getsize(lists_file) > 0:
     lists = load_from_json_file(lists_file)
+
+if sys.argv:
+    for item in sys.argv[1:]:
+        lists.append(item)
+save_to_json_file(lists, lists_file)
