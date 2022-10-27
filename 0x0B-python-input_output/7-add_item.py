cdf85dee2e5a6ad1c56d02fbb4a31588c0b99496
Allen Sibanda <allenalaska1@gmail.com>
2022-10-27 03:51:10 -0700

    updated task 7file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Module containing a script that adds all arguments to a Python list,
+    and then save them to a file:
+"""
+
+import os
+import json
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+lists_file = "add_item.json"
+lists  = []
+
+if os.path.exists(lists_file) and os.path.getsize(lists_file) > 0:
+    lists = load_from_json_file(lists_file)
