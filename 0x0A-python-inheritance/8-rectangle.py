2aea76a45b62286e0ec169ae7aad1a84026d031d
Allen Sibanda <allenalaska1@gmail.com>
2022-10-30 04:47:19 -0700

    made corrections to task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,6 +10,11 @@ class Rectangle(BaseGeometry):
     """ Definition for sub class Rectangle """
 
     def __init__(self, width=0, height=0):
+        """Initialiser for new objects
+            Args:
+            width: width of the new object.
+            height: height of the new object.
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
