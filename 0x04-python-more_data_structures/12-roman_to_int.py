39a3bb37a68aee714ff7dac708dcaf6391e66d96
Allen Sibanda <allenalaska1@gmail.com>
2023-01-12 10:19:28 -0800

    resolved pycodestyle errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
-def roman_to_int(roman_string):
-    """
-    Convert a Roman numeral string to an integer.
+#!/usr/bin/python3
+def roman_to_int(roman_string: str) -> int:
+    """Convert a Roman numeral string to an integer.
 
     Parameters:
     roman_string (str): A Roman numeral string.
@@ -11,22 +11,15 @@ def roman_to_int(roman_string):
     """
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
-    roman_to_int = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+    roman_to_int_d: dict[str, int] = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     result = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_to_int[roman_string[i]] >
-        roman_to_int[roman_string[i - 1]]:
-            result += roman_to_int[roman_string[i]] - 2 *
-            roman_to_int[roman_string[i - 1]]
+        if (i > 0 and roman_to_int_d[roman_string[i]]) > \
+                roman_to_int_d[roman_string[i-1]]:
+            result += roman_to_int_d[roman_string[i]] - 2 *\
+                      roman_to_int_d[roman_string[i-1]]
         else:
-            result += roman_to_int[roman_string[i]]
+            result += roman_to_int_d[roman_string[i]]
     return result
