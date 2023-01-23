6f4a099d4a3a5aba6cbd023ae15b8a887c6822f9
Allen Sibanda <allenalaska1@gmail.com>
2023-01-23 23:45:51 +0200

    added proper handling of index error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             index += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
+        except IndexError:
+            print("IndexError: list index out of range")
     print()
     return index
