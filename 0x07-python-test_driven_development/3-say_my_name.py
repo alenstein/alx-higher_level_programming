1b4f8dab81a7f4c26f8bcee1e7ce836fa94c23c2
Allen Sibanda <allenalaska1@gmail.com>
2023-02-05 18:22:56 +0200

    completed task3 and wrote doctests for it

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" definition  for say_my_name function."""
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints My name is <first name> <last name> <first
+    parameters:
+        first_name: string, first param
+        last_name: string, second param
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
