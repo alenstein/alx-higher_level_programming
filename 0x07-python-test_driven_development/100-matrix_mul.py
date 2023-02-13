47fe84b9c0d35040bba1f745ee70432b561a0886
Allen Sibanda <allenalaska1@gmail.com>
2023-02-13 11:13:44 +0200

    finished matrix multiplication task

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -4,7 +4,7 @@
 
 def validate_list(the_list):
     """
-    function validates if a list meet the project requirements.
+    function validates if a list meets the project requirements.
 
     Parameter(s):
         the_list: (list) list to be tested.
@@ -20,11 +20,13 @@ def validate_list(the_list):
         if type(item) is not list:
             raise TypeError(name + "must be a list of lists.")
         for element in item:
-            if element is int or element is float:
+            if element not in  [float, int]:
                 raise ValueError(name + " should contain only integers"
                                         " or floats")
         if len(item) is not len(the_list[0]):
             raise TypeError(name + " each row of m_a must be of the same size")
+
+
 
 
 def matrix_mul(m_a, m_b):
@@ -38,13 +40,33 @@ def matrix_mul(m_a, m_b):
      Returns:
            a new matrix representing the product of teh two matrices.
     """
-    m_a_m_b = [[0, 0], [0, 0]]
-    validate_list(m_a)
-    validate_list(m_b)
+    m_a_m_b =  []
+
+    rows = len(m_b)
+    cols = len(m_a[0])
+
+    # test if matrix multiplication is possible.
+    if not (len(m_b) == len(m_a[0])):
+        raise ValueError("")
+
+    print("rows = " + str(rows))
+    print("cols = " + str(cols))
+    for i in range(rows):
+        m_a_m_b.append([])
+    print(m_a_m_b)
+    for j in range(rows):
+        for k in range(cols):
+            print(j, k)
+            print("cols: {:d}  rows: {:d} ".format(cols, rows))
+            m_a_m_b[j].append(0)
 
     print("lists have been validated!")
-    for row in range(len(m_a)):
-        for col in range(len(m_a[0])):
-            print("[{:d}][{:d}]".format(row, col))
-            m_a_m_b[row][col] += (m_a[row][col] * m_b[row][col])
+    for row in range(rows):
+        for col in range(cols):
+            for k in range(rows):
+                print("[{:d}][{:d}][:d]".format(row, col, k))
+                try:
+                    m_a_m_b[row][col] += (m_a[row][k] * m_b[k][col])
+                except IndexError:
+                    print("out of indexes!")
     return m_a_m_b
