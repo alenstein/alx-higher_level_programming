40f7a6aa78677aaadf3ced8ea5c2ddec8a66f71d
Allen Sibanda <allenalaska1@gmail.com>
2023-02-13 11:15:43 +0200

    reviewed and cleaned the code

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -49,24 +49,19 @@ def matrix_mul(m_a, m_b):
     if not (len(m_b) == len(m_a[0])):
         raise ValueError("")
 
-    print("rows = " + str(rows))
-    print("cols = " + str(cols))
     for i in range(rows):
         m_a_m_b.append([])
     print(m_a_m_b)
     for j in range(rows):
         for k in range(cols):
             print(j, k)
-            print("cols: {:d}  rows: {:d} ".format(cols, rows))
             m_a_m_b[j].append(0)
 
-    print("lists have been validated!")
     for row in range(rows):
         for col in range(cols):
             for k in range(rows):
-                print("[{:d}][{:d}][:d]".format(row, col, k))
                 try:
                     m_a_m_b[row][col] += (m_a[row][k] * m_b[k][col])
                 except IndexError:
-                    print("out of indexes!")
+                    print()
     return m_a_m_b
