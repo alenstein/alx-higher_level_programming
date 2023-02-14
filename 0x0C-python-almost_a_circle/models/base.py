55da8ffeb36b5d59100598bb83697f2d84deda84
Allen Sibanda <allenalaska1@gmail.com>
2023-02-14 08:23:36 +0200

    initial commit for project 0x0C

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Module containing definition for class base"""
+
+
+class Base:
+    """
+    The base class for all other classes in this project.
+
+    The goal of this class is to manage the id attribute in all future classes
+    and to avoid duplicating the same code and bugs.
+
+    Attributes:
+        __nb_objects (int): A private class attribute that keeps track of the
+            number of instances of the class that have been created.
+
+        id (int): A public instance attribute that represents the unique
+            identifier of each instance of the class.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        """
+        Initialize a new instance of the Base class.
+
+        Parameters:
+            id (int, optional): The unique identifier for the instance.
+                If not provided, a new identifier is generated based on the
+                number of instances that have already been created.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,124 @@
+#!/usr/bn/python3
+""" Module containing the definition for class Rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class that represents a rectangle.
+
+    Attributes:
+        id (int): A unique identifier for the instance.
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The horizontal position of the rectangle.
+        y (int): The vertical position of the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The horizontal position of the rectangle.
+                Defaults to 0.
+            y (int, optional): The vertical position of the rectangle.
+                Defaults to 0.
+            id (int, optional): The unique identifier for the instance.
+                Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """int: The width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not positive.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be positive")
+        self.__width = value
+
+    @property
+    def height(self):
+        """int: The height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not positive.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be positive")
+        self.__height = value
+
+    @property
+    def x(self):
+        """int: The horizontal position of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the horizontal position of the rectangle.
+
+        Args:
+            value (int): The new horizontal position of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not negative.
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x cannot be negative")
+        self.__x = value
+
+    @property
+    def y(self):
+        """int: The vertical position of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the vertical position of the rectangle.
+
+        Args:
+            value (int): The new vertical position of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not negative.
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y cannot be negative")
+        self.__y = value
