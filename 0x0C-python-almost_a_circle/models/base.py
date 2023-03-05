fdf2bd53f85b818d5ce75ac285c322a4f0a01ff4
Allen Sibanda <allenalaska1@gmail.com>
2023-03-05 22:35:19 +0200

    added update method, str representaion and more methods to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,34 +1,84 @@
 #!/usr/bin/python3
-""" Module containing definition for class base"""
+"""Module that contains definition for Base class of the project"""
+
+import json
 
 
 class Base:
-    """
-    The base class for all other classes in this project.
-    The goal of this class is to manage the id attribute in all future classes
-    and to avoid duplicating the same code and bugs.
-
-    Attributes:
-        __nb_objects (int): A private class attribute that keeps track of the
-            number of instances of the class that have been created.
-
-        id (int): A public instance attribute that represents the unique
-            identifier of each instance of the class.
-    """
-
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Initialize a new instance of the Base class.
-
-        Parameters:
-            id (int, optional): The unique identifier for the instance.
-                If not provided, a new identifier is generated based on the
-                number of instances that have already been created.
-        """
+        """initializes a new instance of the Base class."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantiated from a dictionary of attributes.
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
+        Reads from `<cls.__name__>.json`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
