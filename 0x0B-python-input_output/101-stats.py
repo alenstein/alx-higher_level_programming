be0cecae0fed6b9f70f93fba3d6a2c377b4e18db
Allen Sibanda <allenalaska1@gmail.com>
2023-03-05 19:11:06 +0200

    debugged mandatory tasks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,55 +1,44 @@
-#!/usr/bin/env python3
-""" Module containing script that reads stdin line by line and
-    computes metrics:
+#!/usr/bin/python3
+""" Module containing code that reads stdin line by line and computes metrics
 """
 import sys
 
-TOTAL_COUNTS = {'total': 0, '200': 0, '301': 0, '400': 0, '401': 0,
-                '403': 0, '404': 0, '405': 0, '500': 0}
-TOTAL_FILE_SIZE = 0
+# Initialize variables for the metrics.
+total_size = 0
+status_counts = {}
 
+try:
+    # Read lines from standard input.
+    for i, line in enumerate(sys.stdin, start=1):
+        # Parse the fields from the input line.
+        parts = line.split()
+        ip_address, date, method, path, protocol, status_code, file_size = parts
 
-def print_metrics(counts, file_size):
-    """
-    Prints the metrics for a given set of counts and file size
-    """
-    print("File size: {}".format(file_size))
-    for code, count in counts.items():
-        print("{}: {}".format(code, count))
+        # Convert the file size to an integer.
+        file_size = int(file_size)
 
+        # Add the file size to the total size.
+        total_size += file_size
 
-def process_line(line=""):
-    """
-    Processes a single log line and updates the counts and file size
-    """
-    global TOTAL_FILE_SIZE
-    global TOTAL_COUNTS
+        # Increment the count for the status code.
+        if status_code in status_counts:
+            status_counts[status_code] += 1
+        else:
+            status_counts[status_code] = 1
 
-    # Parse the line
-    fields = line.split()
-    if len(fields) < 7:
-        print("Invalid line format: {}".format(line.strip()))
-        return
+        # Print the metrics every 10 lines.
+        if i % 10 == 0:
+            print(f"Total file size: File size: {total_size}")
+            for code in sorted(status_counts.keys()):
+                print(f"{code}: {status_counts[code]}")
 
-    try:
-        status_code = fields[-2]
-        file_size = fields[-1]
-        TOTAL_FILE_SIZE += int(file_size)
-        TOTAL_COUNTS['total'] += 1
-        if status_code in TOTAL_COUNTS:
-            TOTAL_COUNTS[status_code] += 1
-    except ValueError:
-        print("Invalid line format: {}".format(line.strip()))
-        return
+    # Print the final metrics.
+    print(f"Total file size: File size: {total_size}")
+    for code in sorted(status_counts.keys()):
+        print(f"{code}: {status_counts[code]}")
 
-    # Print the metrics every 10 lines
-    if TOTAL_COUNTS['total'] % 10 == 0:
-        print_metrics(TOTAL_COUNTS, TOTAL_FILE_SIZE)
-
-
-# Process each line from stdin
-for line in sys.stdin:
-    process_line(line)
-
-# Print the final metrics
-print_metrics(TOTAL_COUNTS, TOTAL_FILE_SIZE)
+except KeyboardInterrupt:
+    # Print the final metrics on keyboard interrupt.
+    print(f"Total file size: File size: {total_size}")
+    for code in sorted(status_counts.keys()):
+        print(f"{code}: {status_counts[code]}")
